36ae3103ee711ffe7d387e8b274432d791e9523f
Florian Merle <florian.david.merle@gmail.com>
2021-09-14 13:50:06 +0200

    feat: Load reviews using ajax

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const nodeSass = require('node-sass');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const minify = require('gulp-minify');
 const del = require('del');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
@@ -27,6 +27,7 @@ gulp.task('minify-css', () => {
 gulp.task('clean', () => {
   return del([
     '../src/Resources/public/css/*',
+    '../src/Resources/public/js/*',
   ], {
     force: true,
   });
@@ -34,12 +35,22 @@ gulp.task('clean', () => {
 
 gulp.task('script', () => {
   return gulp.src('js/**/*.js')
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
     .pipe(gulp.dest('../src/Resources/public/js/'));
 });
 
-gulp.task('default', gulp.series(['clean', 'styles', 'minify-css', 'script']));
+gulp.task('minify-script', () => {
+  return gulp.src('js/**/*.js')
+    .pipe(uglify())
+    .pipe(minify({
+      ext:{
+        min:'.min.js'
+      },
+      noSource: true,
+    }))
+    .pipe(gulp.dest('../src/Resources/public/js/'));
+});
+
+gulp.task('default', gulp.series(['clean', 'styles', 'minify-css', 'script', 'minify-script']));
 
 gulp.task('watch', () => {
   gulp.watch('sass/**/*.scss', (done) => {
